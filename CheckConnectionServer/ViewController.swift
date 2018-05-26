ec3fd4674446aaa27c38e821eb078c41e802bbe8
rchampa <ricardo.champa@gmail.com>
2018-05-26 13:09:35 +0200

    Initial Commit

---
+++ CheckConnectionServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckConnectionServer
+//
+//  Created by Ricardo Champa on 26/05/2018.
+//  Copyright © 2018 rchampa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
